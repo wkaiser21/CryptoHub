6008a497e26e1fdff6930b91db7f914032390e01
Nizom Djuraev <nd596@drexel.edu>
2022-11-12 18:17:39 -0500

    Minor error fix in setup.sql

--- CryptoHub/setup.sql
+++ CryptoHub/setup.sql
@@ -8,16 +8,16 @@ INSERT INTO users (username, password) VALUES ('nd596', 'testPass');
 
 CREATE TABLE portfolio1 (
 	username VARCHAR(25) UNIQUE,
-	coin CHAR(25),
+	coin CHAR(10),
 	amount INT,
 	value DECIMAL(10,2),
 	date TIMESTAMP
 );
-INSERT INTO portfolio (username, coin, amount, value, date) VALUES ('nd596', 'bitcoin', 2, 20000, current_timestamp);
+INSERT INTO portfolio1 (username, coin, amount, value, date) VALUES ('nd596', 'bitcoin', 2, 20000, current_timestamp);
 
 CREATE TABLE portfolio2 (
 	username VARCHAR(25),
-	coin CHAR(25),
+	coin CHAR(10),
 	amount INT,
 	value DECIMAL(10,2),
 	date TIMESTAMP
