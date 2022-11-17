edce75876a11d04c0352aaab97a37992afad35ea
Nizom Djuraev <nd596@drexel.edu>
2022-11-17 17:41:04 -0500

    Updated Portfolio Page
    Portfolio.js/Portfolio.html
    --Portfolio page displays logged in user from stored cookie.

--- CryptoHub/app/public/portfolio.js
+++ CryptoHub/app/public/portfolio.js
@@ -1,5 +1,6 @@
 let table = document.getElementById("portfolioTable");
-
+let pName = document.getElementById("pName");
+console.log(pName);
 fetch("/portfolio", {
     method: "POST",
     headers: {
@@ -9,12 +10,20 @@ fetch("/portfolio", {
         // username: usernameInput.value,
         // plaintextPassword: passwordInput.value,
     })
-}).then((response) => {
+}).then(response => {
     if (response.status === 200) {
         console.log("Portfolio(s) found");
-        
-        
+        return response.json();
     } else {
         console.log("No portfolios found");
     }
-});
+}).then(data => {
+    if(data.username != undefined) {
+        pName.textContent = (data.username + "'s" + " Portfolio");
+        console.log(data.username);
+    } else {
+        pName.textContent = ("User Not Logged In, Please Log In");
+    }
+})
+
+
