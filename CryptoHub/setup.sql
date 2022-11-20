b38bfe711f4530585fa6cba6ec9a0455c1bd50bd
Wyatt Kaiser <wyatt.kaiser@outlook.com>
2022-11-19 21:48:41 -0500

    Update to setup.sql tables
    Creating individual portfolio tables on start

--- CryptoHub/setup.sql
+++ CryptoHub/setup.sql
@@ -16,3 +16,21 @@ CREATE TABLE portfolio (
 );
 INSERT INTO portfolio (username, portfolio, coin, amount, value, date) VALUES ('nd596', 'Portfolio1', 'bitcoin', 2, 20000, current_timestamp);
 INSERT INTO portfolio (username, portfolio, coin, amount, value, date) VALUES ('nd596', 'Portfolio2', 'ethereum', 1, 1500, current_timestamp);
+
+CREATE TABLE portfolio1 (
+	username VARCHAR(25),
+	coin CHAR(10),
+	amount INT,
+	value DECIMAL(10,2),
+	date TIMESTAMP
+);
+INSERT INTO portfolio1 (username, portfolio, coin, amount, value, date) VALUES ('nd596', 'bitcoin', 2, 20000, current_timestamp);
+
+CREATE TABLE portfolio2 (
+	username VARCHAR(25),
+	coin CHAR(10),
+	amount INT,
+	value DECIMAL(10,2),
+	date TIMESTAMP
+);
+INSERT INTO portfolio2 (username, portfolio, coin, amount, value, date) VALUES ('nd596', 'bitcoin', 2, 20000, current_timestamp);
