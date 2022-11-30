b94092ab65fb2dbc8d2fe38b3c8e88fb8ae2a011
Wyatt Kaiser <wyatt.kaiser@outlook.com>
2022-11-29 22:52:18 -0500

    Update to Search Page and Setup
    Removed two tables that were unnecessary from setup.sql. Changed portfolio name select to custom text input rather than dropdown.

--- CryptoHub/setup.sql
+++ CryptoHub/setup.sql
@@ -8,7 +8,7 @@ INSERT INTO users (username, password) VALUES ('nd596', 'testPass');
 
 CREATE TABLE portfolio (
 	username VARCHAR(25),
-	portfolio CHAR(10),
+	portfolio CHAR(25),
 	coin CHAR(10),
 	amount INT,
 	value DECIMAL(10,2),
@@ -16,21 +16,3 @@ CREATE TABLE portfolio (
 );
 INSERT INTO portfolio (username, portfolio, coin, amount, value, date) VALUES ('nd596', 'Portfolio1', 'bitcoin', 2, 20000, current_timestamp);
 INSERT INTO portfolio (username, portfolio, coin, amount, value, date) VALUES ('nd596', 'Portfolio2', 'ethereum', 1, 1500, current_timestamp);
-
-CREATE TABLE portfolio1 (
-	username VARCHAR(25),
-	coin CHAR(10),
-	amount INT,
-	value DECIMAL(10,2),
-	date TIMESTAMP
-);
-INSERT INTO portfolio1 (username, coin, amount, value, date) VALUES ('nd596', 'bitcoin', 2, 20000, current_timestamp);
-
-CREATE TABLE portfolio2 (
-	username VARCHAR(25),
-	coin CHAR(10),
-	amount INT,
-	value DECIMAL(10,2),
-	date TIMESTAMP
-);
-INSERT INTO portfolio2 (username, coin, amount, value, date) VALUES ('nd596', 'bitcoin', 2, 20000, current_timestamp);
